a1cbab645a4a692f11b16b99bd40dedd16ecf5ac
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-12 15:21:40 +0200

    Clean River

--- src/main/kotlin/River.kt
+++ src/main/kotlin/River.kt
@@ -3,32 +3,35 @@ import Direction.North
 class River(private val fishes: List<Fish>) {
     fun survivors(): List<Fish> {
         val survivors = mutableListOf<Fish>()
-
         val oppositeDirection = mutableListOf<Fish>();
         for (fish in fishes) {
             if (fish.direction == North) {
                 oppositeDirection.add(fish);
                 continue;
             }
-            var fishWasEaten = false;
-            var fishAteEachOther = false;
-            while (oppositeDirection.isNotEmpty()) {
-                val nextFish = oppositeDirection.last()
-                if (nextFish.strength > fish.strength) {
-                    fishWasEaten = true;
-                    break;
-                }
-                if (nextFish.strength == fish.strength) {
-                    fishAteEachOther = true;
-                }
-                oppositeDirection.removeLast();
-            }
-            if (!fishWasEaten && !fishAteEachOther) {
+            val fishLives = eatOtherFishesOrDie(fish, oppositeDirection)
+            if (fishLives) {
                 survivors.add(fish);
             }
         }
         survivors.addAll(oppositeDirection);
-
         return survivors
+    }
+
+    private fun eatOtherFishesOrDie(fish: Fish, otherFishes: MutableList<Fish>): Boolean {
+        var fishWasEaten = false;
+        var fishAteEachOther = false;
+        while (otherFishes.isNotEmpty()) {
+            val other = otherFishes.last()
+            if (other.strength > fish.strength) {
+                fishWasEaten = true;
+                break;
+            }
+            if (other.strength == fish.strength) {
+                fishAteEachOther = true;
+            }
+            otherFishes.removeLast();
+        }
+        return !fishWasEaten && !fishAteEachOther
     }
 }
