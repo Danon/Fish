5862e51f157fd90723b568872c814493325fe2a4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-12 15:17:29 +0200

    Clean River

--- src/main/kotlin/River.kt
+++ src/main/kotlin/River.kt
@@ -4,34 +4,31 @@ class River(private val fish: List<Fish>) {
     fun survivors(): List<Fish> {
         val survivors = mutableListOf<Fish>()
 
-        if (fish.size < 2) {
-            survivors.addAll(fish)
-        } else {
-            val temporaryStack = mutableListOf<Fish>();
-            for (fish_ in fish) {
-                if (fish_.direction == SwimsRight) { // fish goes right
-                    temporaryStack.add(fish_);
-                    continue;
-                }
-                var fishWasEaten = false;
-                var fishAteEachOther = false;
-                while (temporaryStack.isNotEmpty()) {
-                    val stackFish = temporaryStack.last()
-                    if (stackFish.strength > fish_.strength) {
-                        fishWasEaten = true;
-                        break;
-                    }
-                    if (stackFish.strength == fish_.strength) {
-                        fishAteEachOther = true;
-                    }
-                    temporaryStack.removeLast();
-                }
-                if (!fishWasEaten && !fishAteEachOther) {
-                    survivors.add(fish_);
-                }
+        val temporaryStack = mutableListOf<Fish>();
+        for (fish_ in fish) {
+            if (fish_.direction == SwimsRight) { // fish goes right
+                temporaryStack.add(fish_);
+                continue;
             }
-            survivors.addAll(temporaryStack);
+            var fishWasEaten = false;
+            var fishAteEachOther = false;
+            while (temporaryStack.isNotEmpty()) {
+                val stackFish = temporaryStack.last()
+                if (stackFish.strength > fish_.strength) {
+                    fishWasEaten = true;
+                    break;
+                }
+                if (stackFish.strength == fish_.strength) {
+                    fishAteEachOther = true;
+                }
+                temporaryStack.removeLast();
+            }
+            if (!fishWasEaten && !fishAteEachOther) {
+                survivors.add(fish_);
+            }
         }
+        survivors.addAll(temporaryStack);
+
         return survivors
     }
 }
