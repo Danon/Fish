d2ddcff0183a1ab1ddc81c554a44c885b54dcaa9
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-12 14:34:47 +0200

    Clean River

--- src/main/kotlin/River.kt
+++ src/main/kotlin/River.kt
@@ -1,16 +1,13 @@
-import Direction.SwimsLeft
+import Direction.SwimsRight
 
 class River(private val fish: List<Fish>) {
     fun survivors(): List<Fish> {
         if (fish.size < 2) {
             return fish
         }
-        if (fish[0].direction == fish[1].direction) {
-            return fish
+        if (fish[0].direction == SwimsRight && fish[0].direction != fish[1].direction) {
+            return listOf(fish.maxByOrNull { it.strength }!!)
         }
-        if (fish[0].direction == SwimsLeft) {
-            return fish
-        }
-        return listOf(fish.maxByOrNull { it.strength }!!)
+        return fish
     }
 }
