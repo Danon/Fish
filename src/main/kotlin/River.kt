5de8acc0ba7d81f10eafc500ca3e7f48117c9ee9
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-12 15:18:01 +0200

    Rename field "fish" to "fishes"

--- src/main/kotlin/River.kt
+++ src/main/kotlin/River.kt
@@ -1,30 +1,30 @@
 import Direction.SwimsRight
 
-class River(private val fish: List<Fish>) {
+class River(private val fishes: List<Fish>) {
     fun survivors(): List<Fish> {
         val survivors = mutableListOf<Fish>()
 
         val temporaryStack = mutableListOf<Fish>();
-        for (fish_ in fish) {
-            if (fish_.direction == SwimsRight) { // fish goes right
-                temporaryStack.add(fish_);
+        for (fish in fishes) {
+            if (fish.direction == SwimsRight) { // fish goes right
+                temporaryStack.add(fish);
                 continue;
             }
             var fishWasEaten = false;
             var fishAteEachOther = false;
             while (temporaryStack.isNotEmpty()) {
                 val stackFish = temporaryStack.last()
-                if (stackFish.strength > fish_.strength) {
+                if (stackFish.strength > fish.strength) {
                     fishWasEaten = true;
                     break;
                 }
-                if (stackFish.strength == fish_.strength) {
+                if (stackFish.strength == fish.strength) {
                     fishAteEachOther = true;
                 }
                 temporaryStack.removeLast();
             }
             if (!fishWasEaten && !fishAteEachOther) {
-                survivors.add(fish_);
+                survivors.add(fish);
             }
         }
         survivors.addAll(temporaryStack);
